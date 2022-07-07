48b6cd54942821c1a1d73e74612421e566a9d555
Marta Cruz <inmerset@gmail.com>
2022-07-07 14:44:23 +0200

    Update ex2-contarValores.php

--- Tema 3/nivell-3/ex2-contarValores.php
+++ Tema 3/nivell-3/ex2-contarValores.php
@@ -11,23 +11,23 @@ function cuenta_valor($array, $valor_buscado) {
     } else 
     {
         //recorremos el array con un foreach
-        $i=0;
+        $contador=0;
         foreach($array as $valor) {
             if ($valor_buscado === $valor){
-                $i++;
+                $contador++;
             }
         }
-        return $i;
+        return $contador;
     }
 
 }
 
-$contador = cuenta_valor($X, $valor_buscado);
+$cant_repeticiones = cuenta_valor($X, $valor_buscado);
 
 echo "<pre>";
 echo 'Teniendo el siguiente array:<br>';
 print_r($X);
-echo 'El número de veces que aparece '.$valor_buscado. ' es '.$contador.'.'; 
+echo 'El número de veces que aparece '.$valor_buscado. ' es '.$cant_repeticiones.'.'; 
 echo "</pre>";
 
 ?>
