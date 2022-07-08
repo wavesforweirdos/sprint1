85ad1cd4c353f7c7b985d29bd910af23ca207c5a
Marta Cruz <inmerset@gmail.com>
2022-07-08 02:06:02 +0200

    Create ex2-escondite.php

---
+++ Tema 3/Funciones y estructuras/ex2-escondite.php
@@ -0,0 +1,15 @@
+<?php
+
+function escondite()
+{
+    for ($i = 0; $i <= 10; $i+2) {
+        $msj = $i.'... <br>';
+
+        if ($i == 10) {
+        $msj =$i.'... YA!'; 
+        }
+    }
+    return $msj;
+}
+$msj = 'Vamos a jugar al escondite. <br>';
+$msj .= escondite();
