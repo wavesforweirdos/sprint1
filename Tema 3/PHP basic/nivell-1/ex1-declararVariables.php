ad2a20ae5a4c43104fc429f7409640bc42bdf612
Marta Cruz <inmerset@gmail.com>
2022-07-08 02:29:32 +0200

    Create ex1-declararVariables.php

---
+++ Tema 3/PHP basic/nivell-1/ex1-declararVariables.php
@@ -0,0 +1,15 @@
+
+<?php
+//declaración e inicialización
+$integer = 31;
+$double_or_float = 178.10;
+$string  = 'Marta';
+$boolean  = true;
+
+//imprimir por pantalla
+echo $integer . '<br>' . $double_or_float . '<br>' . $string . '<br>' . $boolean . '<hr>';
+var_dump($integer);
+var_dump($double_or_float);
+var_dump($string);
+var_dump($boolean);
+?>
