2637fde1e666fc88bf51f70cc889e5a408331589
Marta Cruz <inmerset@gmail.com>
2022-07-08 02:29:24 +0200

    Create ex3-esconditeConParam.php

---
+++ Tema 3/Funciones y estructuras/ex3-esconditeConParam.php
@@ -0,0 +1,23 @@
+<?php
+
+$max_num = mt_rand(1, 50); //número aleatorio hasta el 50
+
+function escondite($num)
+{
+    $msj = 'Vamos a jugar al escondite. ¡Contaré hasta '.$num.'! <br>';
+
+    for ($i = 0; $i <= $num; $i += 2) {
+
+        if ($i < $num) {
+            $msj .= $i . '... ';
+        } else {
+            $msj .= $i . '... YA!';
+        }
+    }
+    return $msj;
+}
+
+$msj = escondite($max_num);
+echo $msj;
+
+?>
