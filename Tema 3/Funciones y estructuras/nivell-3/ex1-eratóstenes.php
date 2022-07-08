bb66ebcbd2af9a753970625d7a10f12437e9a058
Marta Cruz <inmerset@gmail.com>
2022-07-08 18:56:37 +0200

    Update ex1-eratóstenes.php

--- Tema 3/Funciones y estructuras/nivell-3/ex1-eratóstenes.php
+++ Tema 3/Funciones y estructuras/nivell-3/ex1-eratóstenes.php
@@ -1,2 +1,46 @@
 <?php
-$numero = mt_rand(2,200);
+$numero = (int)mt_rand(2, 200); 
+    //(int) me aseguro la conversión a entero de cualquier número introducido
+$lista_numeros = 0;
+
+
+function erastostenes($num)
+{
+for ($i=2; $i <= $num ; $i++) { 
+
+    if ($num<0) {
+    # code...
+}}
+
+}
+
+echo $numero;
+
+/*
+ function esPrimo() {
+            let numeroIndicado = parseInt(document.getElementById('numero').value);
+            let vPrimo; 
+            let msj ='';
+
+            if (numeroIndicado == 0 || numeroIndicado == 1) {
+                msj = 'El número ' + numeroIndicado + ' no se clasifica.';
+            } else if (numeroIndicado < 0) {
+                msj = 'Los números negativos no se clasifican.';
+            } else if (numeroIndicado == 2) {
+                msj = 'El número ' + numeroIndicado + ' es primo.'
+            } else if (numeroIndicado>2){
+                for (let i = 2; i < numeroIndicado; i++) {
+                    console.log('El ' + numeroIndicado + ' % ' + i + ' = ' + numeroIndicado % i)
+                    if (numeroIndicado % i == 0) {
+                        msj = 'El número ' + numeroIndicado + ' es compuesto.'
+                    } else {
+                        msj = 'El número ' + numeroIndicado + ' es primo.';
+                    }
+                }
+            } else {
+                msj = 'Indica un número.'
+            }
+            
+            document.getElementById('parrafo2').innerHTML = msj;
+        }
+        */
