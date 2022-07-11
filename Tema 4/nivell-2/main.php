0da97b34ea4c738ce435867083ef53d5684c40b5
Marta Cruz <inmerset@gmail.com>
2022-07-11 10:57:45 +0200

    Create main.php

---
+++ Tema 4/nivell-2/main.php
@@ -0,0 +1,34 @@
+    <?php
+    require_once "pokerDice.php";
+
+    //Creación y asignación de objetos
+    $dado1 = new PokerDice();
+    $tirada = $dado1->throw();
+    $dado1->shapeName($tirada);
+
+    echo '<br>';
+
+    $dado2 = new PokerDice();
+    $tirada = $dado2->throw();
+    $dado2->shapeName($tirada);
+
+    echo '<br>';
+
+    $dado3 = new PokerDice();
+    $tirada = $dado3->throw();
+    $dado3->shapeName($tirada);
+
+    echo '<br>';
+
+    $dado4 = new PokerDice();
+    $tirada = $dado4->throw();
+    $dado4->shapeName($tirada);
+
+    echo '<br>';
+
+    $dado5 = new PokerDice();
+    $tirada = $dado5->throw();
+    $dado5->shapeName($tirada);
+
+    
+    ?>
