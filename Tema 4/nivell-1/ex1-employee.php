135d799b9a3c11a8e77221b8d3f249eccca615fe
Marta Cruz <inmerset@gmail.com>
2022-07-11 10:57:40 +0200

    Create ex1-employee.php

---
+++ Tema 4/nivell-1/ex1-employee.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+    <title>Clases</title>
+</head>
+
+<body>
+
+
+    <?php
+
+    class Employee
+    {
+        //atributos 
+        private $nombre;
+        private $sueldo;
+
+        //métodos
+        public function initialize($nombre, $sueldo)
+        {
+            $this->nombre = $nombre;
+            $this->sueldo = $sueldo;
+        }
+
+        public function print()
+        {
+            echo $this->nombre;
+            if ($this->sueldo > 6000) {
+                echo ' tiene que pagar impuestos.';
+            } else {
+                echo ' no tiene que pagar impuestos.';
+            }
+        }
+    }
+
+    //Creación y asignación de objetos
+    $persona = new Employee();
+    $persona->initialize('Marta', 36651);
+    $persona->print();
+
+
+    ?>
+
+</body>
+
+</html>
