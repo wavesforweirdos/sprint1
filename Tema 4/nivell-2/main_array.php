bfd2bb495a240fc75fa257d279f073d428328cf7
Marta Cruz <inmerset@gmail.com>
2022-07-12 13:02:15 +0200

    Create main_array.php

---
+++ Tema 4/nivell-2/main_array.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./hojadestilos.css" rel="stylesheet" type="text/css">
+    <title>Aplicación de Dados</title>
+</head>
+
+<body>
+    <div class="container">
+    <?php
+    require_once "pokerDice_array.php";
+
+    $dado1 = new PokerDice();
+    $dado1->throw();
+    echo $dado1->shapeName();
+
+    $dado2 = new PokerDice();
+    $dado2->throw();
+    echo $dado2->shapeName();
+    
+    $dado3 = new PokerDice();
+    $dado3->throw();
+    echo $dado3->shapeName();
+    
+    $dado3 = new PokerDice();
+    $dado3->throw();
+    echo $dado3->shapeName();
+
+    $dado3 = new PokerDice();
+    $dado3->throw();
+    echo $dado3->shapeName();
+    ?>
+    </div>
+</body>
+
+</html>
