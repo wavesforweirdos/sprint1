240c4e630a1781bbff201df3635f57b181b11f63
Marta Cruz <inmerset@gmail.com>
2022-07-12 13:07:48 +0200

    Update main_array.php

--- Tema 4/nivell-2/main_array.php
+++ Tema 4/nivell-2/main_array.php
@@ -15,23 +15,18 @@
     require_once "pokerDice_array.php";
 
     $dado1 = new PokerDice();
-    $dado1->throw();
     echo $dado1->shapeName();
 
     $dado2 = new PokerDice();
-    $dado2->throw();
     echo $dado2->shapeName();
     
     $dado3 = new PokerDice();
-    $dado3->throw();
     echo $dado3->shapeName();
     
     $dado4 = new PokerDice();
-    $dado4->throw();
     echo $dado4->shapeName();
 
     $dado4 = new PokerDice();
-    $dado4->throw();
     echo $dado4->shapeName();
     ?>
     </div>
