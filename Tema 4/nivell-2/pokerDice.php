1f11dbede272a6029399940349d200ce8c284226
Marta Cruz <inmerset@gmail.com>
2022-07-13 09:57:47 +0200

    Update pokerDice.php

--- Tema 4/nivell-2/pokerDice.php
+++ Tema 4/nivell-2/pokerDice.php
@@ -3,11 +3,11 @@ class PokerDice
 {
     private $shapeDice = array("As", "K", "Q", "J", 7, 8);
     public $selectedShape;
-    public static $counter = 0; //De inicio habremos tirado 0 veces el ocnjunto de dados (+1 cada vez que lancemos)
+    public static $counter = 0; //De inicio habremos tirado 0 veces el conjunto de dados (+1 cada vez que lancemos)
 
     public function throw()
     {
-        $this->selectedShape = array_rand($this->shapeDice,1);
+        $this->selectedShape = array_rand($this->shapeDice, 1);
         //array_rand selecciona 1 valor/objeto aleatorio del array y retorna el índice de este
         return $this->selectedShape;
     }
@@ -15,14 +15,14 @@ class PokerDice
     public function shapeName()
     {
 
-        echo '<div class="dado">'. $this->shapeDice[$this->throw()] . '</div>';   
+        echo '<div class="dado">' . $this->shapeDice[$this->throw()] . '</div>';
     }
 
 
-    public static function letsPlay()  
-    
+    public static function letsPlay()
+
     //palabra reservada static => Declarar propiedades o métodos de clases como estáticos los hacen accesibles sin la necesidad de instanciar la clase. Un método estático puede ser accedido sin un objeto de clase instanciado (una propiedad no puede).
-    
+
     {
         $numDices = 5;
 
@@ -30,11 +30,12 @@ class PokerDice
             $dado = new PokerDice();
             echo $dado->shapeName();
         }
-        self::$counter += 1;
+        self::$counter += 1;//cada vez que lanzamos los 5 dados, augmentamos el contador en 1
     }
 
 
-    public static function getTotalThrows(){
-        return self::$counter;
+    public static function getTotalThrows()
+    {
+               return self::$counter;
     }
 }
