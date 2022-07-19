4ea0ca436de00598a381abbc56e09608ebcd832a
Marta Cruz <inmerset@gmail.com>
2022-07-19 19:05:16 +0200

    Update ex4-esconditePorDefecto.php

--- Tema 3/Funciones y estructuras/nivell-1/ex4-esconditePorDefecto.php
+++ Tema 3/Funciones y estructuras/nivell-1/ex4-esconditePorDefecto.php
@@ -1,8 +1,8 @@
 <?php
 
-function escondite($num)
+function escondite($num = 10)
 {
-    $msj = 'Vamos a jugar al escondite. ¡Contaré hasta '.$num.'! <br>';
+    $msj = 'Vamos a jugar al escondite. ¡Contaré hasta ' . $num . '! <br>';
 
     for ($i = 0; $i <= $num; $i += 2) {
 
@@ -15,7 +15,15 @@ function escondite($num)
     return $msj;
 }
 
-$msj = escondite(10);
-echo $msj;
+/* Podemos pasarle un parámetro a la función y usará este valor */
+    $msj = escondite(20);
+    echo $msj;
+    
+    echo '<br><br>';
+
+/* en caso contrario, sino especificamos un valor, utilizará el valor por defecto de la función*/
+    $msj = escondite();
+    echo $msj;
+
 
 ?>
