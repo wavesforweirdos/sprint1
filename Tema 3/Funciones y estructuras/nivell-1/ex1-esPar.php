51d4d0aae5fed692a800a47b79db6db6033346f2
Marta Cruz <inmerset@gmail.com>
2022-07-19 19:05:05 +0200

    Update ex1-esPar.php

--- Tema 3/Funciones y estructuras/nivell-1/ex1-esPar.php
+++ Tema 3/Funciones y estructuras/nivell-1/ex1-esPar.php
@@ -2,19 +2,26 @@
 $random = mt_rand(0, 100); //crea un número aleatorio hasta el 100
 $msj = 'El número aleatorio es <b>' . $random . '</b> ';
 
-function esPar($num,$msj)
+
+function esPar($num){
+if ($num % 2){
+    return false;
+}else{
+    return true;
+}
+}
+
+function mostrarMsj($num,$msj)
 {
-    if ($num == 0) {
+    if ($num === 0) {
         $msj .= 'y es <b>par</b> aunque habrá quien lo cuestione.';
-    } elseif ($num % 2 == 0) {
+    } elseif (esPar($num)) {
         $msj .= 'y es <b>par.</b>';
-    } elseif ($num % 2 != 0) {
+    } else {
         $msj .= 'y es <b>impar.</b>';
     }
 
     return $msj;
 }
 
-echo esPar($random,$msj);
-
-?>
+echo mostrarMsj($random,$msj);
