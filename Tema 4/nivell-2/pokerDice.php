81b78ea43a3473af541adbf5c4efe3927e5d9de8
Marta Cruz <inmerset@gmail.com>
2022-07-20 12:39:48 +0200

    Update pokerDice.php

--- Tema 4/nivell-2/pokerDice.php
+++ Tema 4/nivell-2/pokerDice.php
@@ -1,30 +1,14 @@
 <?php
-class PokerDice
-{
-    private $shapeDice = array("As", "K", "Q", "J", 7, 8);
-    public $selectedShape;
+
+//palabra reservada static => Declarar propiedades o métodos de clases como estáticos los hacen accesibles sin la necesidad de instanciar la clase. 
+//Un método estático puede ser accedido sin un objeto de clase instanciado (una propiedad no puede).
+
+class Game{
+
     public static $counterSet = 0; //De inicio habremos tirado 0 veces el conjunto de dados (+1 cada vez que lancemos)
     public static $counterDice = 0; //De inicio habremos tirado 0 veces el dado
-
-    public function throw()
-    {
-        $this->selectedShape = array_rand($this->shapeDice, 1);
-        //array_rand selecciona 1 valor/objeto aleatorio del array y retorna el índice de este
-        return $this->selectedShape;
-    }
-
-    public function shapeName()
-    {
-
-        echo '<div class="dado">' . $this->shapeDice[$this->throw()] . '</div>';
-    }
-
-
+    
     public static function letsPlay()
-
-    //palabra reservada static => Declarar propiedades o métodos de clases como estáticos los hacen accesibles sin la necesidad de instanciar la clase. 
-    //Un método estático puede ser accedido sin un objeto de clase instanciado (una propiedad no puede).
-
     {
         $numDices = 5;
 
@@ -36,14 +20,32 @@ class PokerDice
         self::$counterSet += 1;//cada vez que lanzamos los 5 dados, augmentamos el contador en 1
     }
 
+    public static function getThrows()
+    {
+               return self::$counterSet;
+    }
 
     public static function getTotalThrows()
     {
                return self::$counterDice;
     }
+}
 
-    public static function getThrows()
+
+class PokerDice extends Game
+{
+    private $shapeDice = array("As", "K", "Q", "J", 7, 8);
+    public $selectedShape;
+
+    public function throw()
     {
-               return self::$counterSet;
+        $this->selectedShape = array_rand($this->shapeDice, 1);
+        //array_rand selecciona 1 valor/objeto aleatorio del array y retorna el índice de este
+        return $this->selectedShape;
+    }
+
+    public function shapeName()
+    {
+        echo '<div class="dado">' . $this->shapeDice[$this->throw()] . '</div>';
     }
 }
