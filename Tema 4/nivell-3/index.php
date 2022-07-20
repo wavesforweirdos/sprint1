9a3d4d14103867d488bd5034892c98a08c387279
Marta Cruz <inmerset@gmail.com>
2022-07-20 09:38:18 +0200

    Update index.php

--- Tema 4/nivell-3/index.php
+++ Tema 4/nivell-3/index.php
@@ -10,17 +10,27 @@
 </head>
 
 <body>
+
     <?php
     require_once "account.php";
-    $cliente = new Account('Marta', 'Cruz Quimasó', 'ABC', 1900);
-    echo 'Cliente cargado <p><pre>';
-    var_dump($cliente);
-    echo '</pre></p>';
+
+    session_start();
+    $cliente =  new Account('Marta', 'Cruz Quimasó', 'ABC', 1900);
+    if ((empty($_GET))) {
+        $contador = 1; //valor inicial
+        echo '<h3>Cliente registrado (Registro nº1)</h3><p><pre>';
+        var_dump($cliente);
+        echo '</pre></p>';
+    } else { //si se ha enviado el formulario
+        $contador = $_GET["count"] + 1; //augmento en 1 el contador para los registros
+        $cliente = $_SESSION['cliente']; //declaro mi clase de la sesion
+    }
     ?>
 
-<div>
-    <form method="get">
-            <input type="number" name="amount" step="any" placeholder="€" required>
+    <div>
+        <form method="get">
+            <input type="hidden" name="count" value="<?php echo $contador ?>">
+            <input type="number" name="amount" step="any" placeholder="€" min="0" required>
             <select name="opcion">
                 <option value="ingresar">Ingresar</option>
                 <option value="retirar">Retirar</option>
@@ -28,17 +38,17 @@
             <button>Operar</button>
         </form>
     </div>
-    
+
     <?php
-    echo '<br>';
-    
     if (!(empty($_GET))) {
+
         $amount = $_GET["amount"];
         $operacion = $_GET["opcion"];
-        
-        echo $cliente->operar($amount, $operacion);
-        echo '<br>Cliente actualizado <p><pre>';
-        var_dump($cliente);
+        $cliente->operar($amount, $operacion);
+
+        $_SESSION['cliente'] = $cliente;
+        echo '<h3>Cliente actualizado (Registro nº' . $contador . ')</h3><p><pre>';
+        var_dump($_SESSION['cliente']);
         echo '</pre>';
     }
     ?>
